8c7e2a94e179b6447f9c4d46abc1a26806237fbe
Julia Santos <juliasantos1304@gmail.com>
2024-02-01 02:44:50 -0300

    Create index.jsx

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,31 @@
+import { Container } from './styles';
+
+import Chart from '../Chart';
+
+const Card = ({ jogador }) => {
+
+    return (
+        <Container>
+            <div className='jogador-img'>
+                <img src={`/jogadores/${jogador.foto}`} alt={jogador.foto} />
+            </div>
+            <div className="jogador-info">
+                <div className="jogador-container">
+                    <p>{jogador.nome}</p>
+                    <p>{`${jogador.posicao} | ${jogador.idade} anos`}</p>
+                </div>
+                <div className="jogador-status">
+                    <div className="status-legend">
+                        <p>Jogos</p>
+                        <p>Gols</p>
+                    </div>
+                    <div className="status-data">
+                        <Chart jogos={jogador.jogos} gols={jogador.gols} />
+                    </div>
+                </div>
+            </div>
+        </Container>
+    );
+}
+
+export default Card;
