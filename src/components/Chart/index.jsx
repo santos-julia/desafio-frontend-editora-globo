d2c9470cb46f040bc383ddeea82d7b715da16dfd
Julia Santos <juliasantos1304@gmail.com>
2024-02-01 02:44:37 -0300

    Create index.jsx

---
+++ src/components/Chart/index.jsx
@@ -0,0 +1,23 @@
+import { Container, StatusBar } from './styles';
+
+const Chart = ({ jogos, gols }) => {
+    const maxJogos = 13;
+    const maxGols = 6;
+    const jogosPercent = ((100 * jogos) / maxJogos).toFixed(0);
+    const golsPercent = ((100 * gols) / maxGols).toFixed(0);
+
+    return (
+        <Container>
+            <div className="jogos-container">
+                <StatusBar width={jogosPercent} color={'#B4DDC1'} />
+                <p>{jogos}</p>
+            </div>
+            <div className="gols-container">
+                <StatusBar width={golsPercent} color={'#72BF44'} />
+                <p>{gols}</p>
+            </div>
+        </Container>
+    );
+}
+
+export default Chart;
